9aceb8335c6b5b33efb931f6bab7737de398044f
matthewpalsich <matthew.palsich@gmail.com>
2019-02-05 08:51:19 -0600

    Initial commit.

---
+++ sample/src/main/java/com/sevenshifts/sideheaderdecorator/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.sevenshifts.sideheaderdecorator
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val names = NamesProvider().getNames()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        names_list.layoutManager = LinearLayoutManager(this)
+        names_list.adapter = MyAdapter()
+
+        val namesHeaderProvider = object : SideHeaderDecorator.HeaderProvider<Char> {
+            override fun getHeader(position: Int) = names[position].first()
+        }
+
+        names_list.addItemDecoration(object : SideHeaderDecorator<Char>(namesHeaderProvider) {
+
+            override fun getHeaderView(header: Char, parent: RecyclerView): View {
+                val textView = LayoutInflater.from(parent.context).inflate(R.layout.header_view, parent, false) as TextView
+                textView.text = header.toString()
+                return textView
+            }
+        })
+    }
+
+    class MyViewHolder(private val view: TextView) : RecyclerView.ViewHolder(view) {
+
+        fun setText(s: String) {
+            view.text = s
+        }
+    }
+
+    inner class MyAdapter : RecyclerView.Adapter<MyViewHolder>() {
+
+        override fun getItemCount() = names.size
+
+        override fun onCreateViewHolder(parent: ViewGroup, position: Int): MyViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+            return MyViewHolder(view as TextView)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+            val name = names[position]
+            holder.setText(name)
+        }
+    }
+}
---
+++ sideheaderdecorator/src/main/java/com/sevenshifts/sideheaderdecorator/SideHeaderDecorator.kt
@@ -0,0 +1,78 @@
+package com.sevenshifts.sideheaderdecorator
+
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import kotlin.math.max
+import kotlin.math.min
+
+abstract class SideHeaderDecorator<H>(private val headerProvider: HeaderProvider<H>) : RecyclerView.ItemDecoration() {
+
+    interface HeaderProvider<H> {
+        fun getHeader(position: Int): H
+    }
+
+    abstract fun getHeaderView(header: H, parent: RecyclerView): View
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+
+        val position = parent.getChildAdapterPosition(view)
+        val headerView = getHeaderView(headerProvider.getHeader(position), parent)
+
+        val wrapContentMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+        val parentWidthMeasureSpec = View.MeasureSpec.makeMeasureSpec(parent.width, View.MeasureSpec.EXACTLY)
+
+        view.measure(parentWidthMeasureSpec, wrapContentMeasureSpec)
+        headerView.measure(wrapContentMeasureSpec, wrapContentMeasureSpec)
+
+        headerView.measuredHeight.takeIf { it > view.measuredHeight }?.let {
+            view.layoutParams = view.layoutParams.apply { height = it }
+        }
+
+        view.apply { setPadding(headerView.measuredWidth, paddingTop, paddingRight, paddingBottom) }
+    }
+
+    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDrawOver(c, parent, state)
+
+        val lastIndex = parent.adapter?.let { it.itemCount - 1 } ?: return
+
+        for (childPosition in (0..parent.childCount)) {
+            val view = parent.getChildAt(childPosition)
+
+            val position = parent.getChildAdapterPosition(view)
+
+            if (position == RecyclerView.NO_POSITION) {
+                continue
+            }
+
+            val header = headerProvider.getHeader(position)
+            val headerView = getHeaderView(header, parent)
+            val wrapContentMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
+            headerView.measure(wrapContentMeasureSpec, wrapContentMeasureSpec)
+            headerView.layout(0, 0, headerView.measuredWidth, headerView.measuredHeight)
+
+            val beginsGroup = (position == 0 || header != headerProvider.getHeader(position - 1))
+            val endsGroup = (position == lastIndex ||header != headerProvider.getHeader(position + 1))
+
+            c.save()
+            if (childPosition == 0) {
+                val top = when {
+                    beginsGroup && endsGroup -> view.top.toFloat()
+                    beginsGroup -> max(view.top, 0).toFloat()
+                    endsGroup -> min(view.bottom - headerView.measuredHeight, 0).toFloat()
+                    else -> 0f
+                }
+
+                c.translate(0f, top)
+                headerView.draw(c)
+            } else if (beginsGroup) {
+                c.translate(0f, view.top.toFloat())
+                headerView.draw(c)
+            }
+            c.restore()
+        }
+    }
+}
